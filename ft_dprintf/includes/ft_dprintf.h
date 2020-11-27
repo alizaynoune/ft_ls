bb8cb1a20d07f9e21c275dcc25f509193b6ab134
alizaynoune <simo.jokir@gmail.com>
2020-11-27 20:56:08 +0100

    update ft_dprintf
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

--- ft_dprintf/includes/ft_dprintf.h
+++ ft_dprintf/includes/ft_dprintf.h
@@ -91,31 +91,4 @@ void					reset_data(t_data *d);
 size_t					ato_unsint_(const char *str);
 int						ft_printf(const char *format, ...);
 
-int						ft_dprintf(int fd, const char *format, ...);
-void					get_arg(t_data *d);
-void					flags_(t_data *d);
-void					width_precision(t_data *d);
-void					length_(t_data *d);
-void					specifier_(t_data *d);
-void					string_(t_data *d, char *str);
-void					char_(t_data *d, int c);
-void					octal_(t_data *d);
-void					binary_(t_data *d);
-char					*unsigned_str_(t_data *d, unsigned long long int value);
-char					*base_str_(t_data *d, unsigned long long int v,
-						char *s_, size_t base);
-void					fix_len_decimal_(t_data *d, char *str);
-void					fix_len_octal_(t_data *d, char *str);
-void					fix_p_(t_data *d, char **str);
-void					loop_char(t_data *d, char c, int loop);
-void					put_hash_(t_data *d);
-void					put_str_(t_data *d, char *str);
-void					put_octal_(t_data *d, char *str);
-void					put_decimal_(t_data *d, char *str);
-void					put_decimal_(t_data *d, char *str);
-void					put_flag_(t_data *d, char c);
-void					put_hexa_(t_data *d, char *str);
-void					exit_error_(t_data *d);
-void					reset_data(t_data *d);
-size_t					ato_unsint_(const char *str);
 #endif
